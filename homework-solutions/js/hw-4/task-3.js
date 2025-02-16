29fba54e93e672a1891d0482a5acb3bbd92f3b07
evgban <bannikovaevg@yandex.ru>
2025-02-16 19:16:49 +0300

    HW-4: change task-3.js

--- homework-solutions/js/hw-4/task-3.js
+++ homework-solutions/js/hw-4/task-3.js
@@ -6,3 +6,23 @@
   преобразовываясь в number
 
 */
+  let minAge = 18;
+  let maxAge = 60;
+  let age;
+  age = "22";
+  let typeOfAgeIsNumber = Number(age);
+  if (typeof typeOfAgeIsNumber != "number") {
+    console.log("Incorrect data type");
+  }
+  else if (typeOfAgeIsNumber < minAge) {
+    console.log("You don't have access cause your age is " + age + "." + " It's less then " + minAge + ".");
+  }
+  else if (minAge <= typeOfAgeIsNumber, typeOfAgeIsNumber < maxAge) {
+    console.log("Welcome !");
+  }
+  else if (typeOfAgeIsNumber > maxAge) {
+    console.log("Keep calm and look Culture channel");
+  }
+  else {
+    console.log("Technical work");
+  }
