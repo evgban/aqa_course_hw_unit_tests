9435b8f711d6227b5c8fade5f2e5f3f91f141bc0
evgban <bannikovaevg@yandex.ru>
2025-02-22 22:25:05 +0300

    HW-6: add task-2

--- homework-solutions/js/hw-6/task-2.mjs
+++ homework-solutions/js/hw-6/task-2.mjs
@@ -13,7 +13,30 @@
   const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
 */
 
-let resultUnique;
-let resultNull;
+let resultUnique = [];
+let resultNull = [];
+const competitorPizzas = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai']
+const myPizzasT1 = ['Peperoni', 'Margherita', 'Diablo', 'Vegetarian'];
+const myPizzasT2 = ['Peperoni', 'Caprichosa', 'Diablo', '4 cheeses', 'hawai'];
+let lowerCaseCompetitorPizzas = [];
+for (let i = 0; i < competitorPizzas.length; i++) {
+  lowerCaseCompetitorPizzas.push(competitorPizzas[i].toLowerCase());
+}
+for (let j of myPizzasT1) {
+  if (lowerCaseCompetitorPizzas.includes(j.toLowerCase())) {
+  resultNull = null;
+} else {
+  resultUnique.push(j);
+}
+}
+for (let k of myPizzasT2) {
+  if (lowerCaseCompetitorPizzas.includes(k.toLowerCase())) {
+  resultNull = null;
+} else {
+  resultUnique.push(k);
+}
+}
+console.log(resultNull);
+console.log(resultUnique);
 
 export { resultNull, resultUnique };
